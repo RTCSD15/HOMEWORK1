ba33184fb7cccd36cf3b3891fb0bf242dfb5249b
sqy5890 <40267648+sqy5890@users.noreply.github.com>
2018-06-14 20:20:25 +0800

    Create myprogram.cpp
    第一次编程作业
    学号：161101217

---
+++ 161101217/myprogram.cpp
@@ -0,0 +1,5 @@
+ touch message.txt
+         sudo mv message.txt /home
+         sudo mv message.txt /home/ubuntu/New_dir
+         cp message.txt /home/ubuntu/New1_dir
+         rm /home/message.txt /home/message.txt/New_dir
